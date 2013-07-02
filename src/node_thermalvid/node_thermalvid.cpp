d0d2eaf468f1851da35441b0e8e9a47398dfb24a
viki <viki@ROS.(none)>
2013-07-02 13:36:43 -0700

    added the thermalvid.cpp file set up webcam in webcam node

---
+++ src/node_thermalvid/node_thermalvid.cpp
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <iostream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cxcore.h>
+#include <opencv/cvblob.h>
+
+#include "CamTrap_Viper/CvService.h"
+#include "cv_localizer.h"
+
+using namespace cvb;
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	const int FLIR_FRAME_WIDTH  = 480;
+   const int FLIR_FRAME_HEIGHT = 320;
+   const int FLIR_FOV_X = 36;
+   const int FLIR_FOV_Y = 27;
+	const double FLIR_WRITER_FRAME_RATE = 29.5;
+
+	int duration_sec = 60 * 5;
+
+	ros::init(argc, argv, "cv_service");
+	ros::NodeHandle n;
+
+	CVLocalizer object_tracker(0, 0, FLIR_FRAME_WIDTH, FLIR_FRAME_HEIGHT, FLIR_FOV_X, FLIR_FOV_Y);
+	object_tracker.setTimestamp(0);
+
+	ros::ServiceServer service = n.advertiseService("cv_service", &CVLocalizer::newCoords, &object_tracker);
+
+	//Initialize Cameras:	
+	// set the norm to NTSC for FLIR - and input to 1 for capture device  
+	system("v4l2-ctl -s NTSC -i 1");   //if FLIR is video0
+	//system("v4l2-ctl -d /dev/video1 -s NTSC -i 1"); if FLIR is video1
+	
+	/* Initialize the IRCam */
+	CvCapture *capture = cvCreateCameraCapture(0);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, FLIR_FRAME_WIDTH);
+	cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, FLIR_FRAME_HEIGHT);
+  
+	/* Always check if the program can find a device */
+	if (!capture)
+	{
+		printf("error 1");
+		ROS_ERROR("Can not open flir");
+		return -1;
+	}
+
+	/* creating display window */
+	//use only for testing
+	cvNamedWindow( "FLIR",CV_WINDOW_AUTOSIZE);
+
+	/* Create required images once */
+	IplImage *img = cvQueryFrame(capture);
+	IplImage  *gray_img = cvCreateImage(cvSize(FLIR_FRAME_WIDTH, FLIR_FRAME_HEIGHT), img->depth, 1);
+ 	IplImage  *thres_img = cvCreateImage(cvSize(FLIR_FRAME_WIDTH, FLIR_FRAME_HEIGHT), img->depth, 1);
+	IplImage  *label_img = cvCreateImage(cvSize(FLIR_FRAME_WIDTH, FLIR_FRAME_HEIGHT), IPL_DEPTH_LABEL, 1);
+
+	/* Time init */
+	time_t rawTime = time (NULL);
+	tm *pTime = gmtime(&rawTime);
+	
+	/* initialize video writer */
+	CvVideoWriter *flirWriter;
+	sprintf(object_tracker.video_name, "%s%d-%d-%d:%d:%d%s", "/home/viki/Videos/thermal/", pTime->tm_mon, pTime->tm_mday, pTime->tm_hour, pTime->tm_min, pTime->tm_sec, ".avi");
+	flirWriter = cvCreateVideoWriter(object_tracker.video_name, CV_FOURCC('D','I','V','X'), FLIR_WRITER_FRAME_RATE, cvSize(FLIR_FRAME_WIDTH, FLIR_FRAME_HEIGHT), 1);
+	
+	/* main loop */
+	int T;
+	double moment10;
+	double moment01;
+	double area;
+	long int timecnt = time(&rawTime) + duration_sec;
+  	static int posX;
+  	static int posY;
+
+while (ros::ok())
+{
+	/* Obtain a frame from the device */
+	img = cvQueryFrame(capture);
+
+	/* Always check if the device returns a frame */
+	if( !img )
+	{
+		ROS_ERROR("Error retrieving FLIR frame\n");
+		return -1;
+	}
+
+	/* remove the watermark */
+	cvSetImageROI(img, cvRect((FLIR_FRAME_WIDTH*367)/480,(FLIR_FRAME_HEIGHT*14)/320,(FLIR_FRAME_WIDTH*90)/480,(FLIR_FRAME_HEIGHT*28)/320));
+	cvZero(img);
+	cvResetImageROI(img);
+
+	/* Time get */
+	rawTime = time (NULL);
+
+	/* reset video writer for every X second */
+	if (timecnt <= time(&rawTime))
+	{
+		cvReleaseVideoWriter(&flirWriter);
+		pTime = gmtime(&rawTime);
+		
+		//object_tracker.video_name;
+		int sprint_test = sprintf(object_tracker.video_name, "%s%d-%d-%d:%d:%d%s", "/home/viki/Videos/thermal/", pTime->tm_mon, pTime->tm_mday, pTime->tm_hour, pTime->tm_min, pTime->tm_sec, ".avi");
+
+		if (sprint_test < 0)
+			ROS_ERROR("sprintf error");
+		
+		flirWriter = cvCreateVideoWriter(object_tracker.video_name, CV_FOURCC('D','I','V','X'), FLIR_WRITER_FRAME_RATE, cvSize(FLIR_FRAME_WIDTH, FLIR_FRAME_HEIGHT), 1);
+		timecnt = time(&rawTime) + duration_sec;
+	}
+
+	// Flipping the img if needed with motor node
+	cvFlip(img, img,-1);
+
+	/* Write images to file */
+	cvWriteFrame(flirWriter, img);
+
+  	/* Convert image from Color to grayscale */
+  	cvCvtColor(img, gray_img, CV_RGB2GRAY); 
+
+	/* Publish videos */
+	cvShowImage( "FLIR", img);
+
+	//for use in node if cvShowImage didn't work
+	/*	cv::Mat flir (img);
+		cv::namedWindow (FLIR, CV_WINDOW_AUTOSIZE);
+		cv::imshow ("FLIR", flir);*/
+
+	/* cleaning memory */
+	cvWaitKey(1);
+	cvZero(img);
+	cvZero(gray_img);
+	cvZero(thres_img);
+	cvZero(label_img);
+
+	ros::spinOnce();
+	}
+ 	
+	/* Clean up memory */
+	//since the loop will break by ros::ok this part is not useable
+	/*	cvReleaseCapture( &capture );
+	cvDestroyAllWindows();*/
+	
+	return 0;
+}
+
+
--- src/node_webcam/node_webcam.cpp
+++ src/node_webcam/node_webcam.cpp
@@ -1,35 +1,124 @@
 #include <stdio.h>
 #include <cv.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <iostream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cxcore.h>
+#include <opencv/cvblob.h>
+#include "CamTrap_Viper/CvService.h"
+#include "cv_localizer.h"
  
-int main(int argc, char * const argv[])
+using namespace cvb;
+using namespace std;
+ 
+int main(int argc, char * argv[])
 {
-	/* Initialize the camera */
-	CvCapture *CamCapture = 0;
-	CamCapture = cvCreateFileCapture("http://192.168.2.135:81/videostream.asf?user=viki&pwd=viki&resolution=640*480");
-	if (!CamCapture)
-	{printf("IP Cam not ready\n");
-	return -1;}
-	/* initialize video writer */
-	CvVideoWriter *IPCamWriter = 0;
-	CvSize size;
-	size.width = 640;
-	size.height = 480;
-	IPCamWriter = cvCreateVideoWriter("/home/viki/Videos/IPCamOut.avi", CV_FOURCC('D','I','V','X'), 4, size, 1); //needs time index in naming
+    const int WEB_FOV_X = 36;
+    const int WEB_FOV_Y = 27;
+    //const int WEB_WRITER_FRAME_RATE = 30;
+    ros::init(argc, argv, "cv_service");
+	 ros::NodeHandle n;
+   
+    int duration_sec = 60 * 5;
 
+	/* Initialize the camera */
+   CvCapture *capture = cvCreateCameraCapture(1);
+	cvQueryFrame(capture);
+
+
+	const int WEB_FRAME_WIDTH = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH);
+	const int WEB_FRAME_HEIGHT = (int)cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT);
+	const double WEB_WRITER_FRAME_RATE = 13.33;//(double)cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
+	//ROS_INFO("img fps:%f", WEB_WRITER_FRAME_RATE);
+   //cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, WEB_FRAME_WIDTH);
+   //cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, WEB_FRAME_HEIGHT);
+//CamCapture = cvCreateFileCapture("http://192.168.2.135:81/videostream.asf?user=viki&pwd=viki&resolution=640*480");
+ //  system("v4l2-ctl -d /dev/video1 -s NTSC -i 1");// if FLIR is video1
+	/* Initialize the WebCam */
+    //cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, WEB_FRAME_HEIGHT);
+ 
+    /* Always check if the program can find a device */
+    if (!capture)
+    {
+       ROS_ERROR("Can not open webcam");
+       return -1;
+    }
+ /* creating display window */
+    //use only for testing
+    cvNamedWindow("WEBCAM", CV_WINDOW_AUTOSIZE);
+ 
+    /* Create required images once */
+    IplImage *img = cvQueryFrame(capture);
+	
+	/* Time init */
+    time_t rawTime = time (NULL);
+    tm *pTime = gmtime(&rawTime);
+
+    char video_name[100];
+	/* initialize video writer */
+   
+    CvVideoWriter *WebWriter;
+    sprintf(video_name, "%s%d-%d-%d:%d:%d%s", "/home/viki/Videos/webcam/", pTime->tm_mon, pTime->tm_mday, pTime->tm_hour, pTime->tm_min, pTime->tm_sec, ".avi");
+
+    WebWriter = cvCreateVideoWriter(video_name, CV_FOURCC('D','I','V','X'), WEB_WRITER_FRAME_RATE, cvSize(WEB_FRAME_WIDTH, WEB_FRAME_HEIGHT), 1);	
+   
 	/* time */
 	time_t current_time;
 	current_time = time (NULL);
 	long int stop_time = current_time + 10;
 	
 	/* main loop */
+    long int timecnt = time(&rawTime) + duration_sec;
+
 	while( current_time < stop_time )
-		{
-		/* write image to file */
-		cvWriteFrame(IPCamWriter, cvQueryFrame( CamCapture ));
-		}
+	{
+ 	/* Obtain a frame from the device */
+   img = cvQueryFrame(capture);
+ 
+    /* Always check if the device returns a frame */
+    if( !img )
+    {
+       ROS_ERROR("Error retrieving webcam frame\n");
+       return -1;
+    }
+    
+	 rawTime = time (NULL);
+ 
+    /* reset video writer for every X second */
+    if (timecnt <= time(&rawTime))
+    {
+       cvReleaseVideoWriter(&WebWriter);
+       pTime = gmtime(&rawTime);
+       int sprint_test = sprintf(video_name, "%s%d-%d-%d:%d:%d%s", "/home/viki/Videos/webcam/", pTime->tm_mon, pTime->tm_mday, pTime->tm_hour, pTime->tm_min, pTime->tm_sec, ".avi");
+       WebWriter = cvCreateVideoWriter(video_name, CV_FOURCC('D','I','V','X'), WEB_WRITER_FRAME_RATE, cvSize(WEB_FRAME_WIDTH, WEB_FRAME_HEIGHT), 1);
+		timecnt = time(&rawTime) + duration_sec;
+	 }
+	
+	cvWriteFrame(WebWriter, cvQueryFrame( capture ));
+ 		
+    /* Publish videos */
+    cvShowImage( "WEBCAM", img);
+    /* cleaning memory */
+    cvWaitKey(1);
+    cvZero(img);
+  
+    ros::spinOnce();
+
+	}
  
 	/* Clean up memory */
-	cvReleaseCapture ( &CamCapture );
-	cvReleaseVideoWriter(&IPCamWriter);
+	cvReleaseCapture ( &capture );
+	cvReleaseVideoWriter(&WebWriter);
 }
