28e1bfd3f70f9039f9af9a7c695cfb227b83a73e
jblackstone <senorblackstone@gmail.com>
2013-08-06 15:11:33 -0700

    add user identification to auto_segment.py

--- auto_segment.py
+++ auto_segment.py
@@ -23,22 +23,25 @@ files = {}
 pygame.init() #user identification
 size = width, height = 600, 400 
 screen = pygame.display.set_mode(size)
-backGround = (255,255,255)
+white = (255,255,255)
 black = (0,0,0)
 pygame.display.set_caption("User Identification")
+screen.fill(black)
 done = False
 clock=pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 display_instructions = True
 instruction_page = 1
-
+screen.fill(black)                
+pygame.display.flip()
 while done == False and display_instructions:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == KEYDOWN and event.key == K_SPACE:
             instruction_page += 1
-            if instruction_page == 1:
+            if instruction_page == 2:
+                done = True                
                 display_instructions = False
         elif event.type == KEYDOWN and event.key == K_j:
             user = "j"
@@ -52,20 +55,21 @@ while done == False and display_instructions:
         elif event.type == KEYDOWN and event.key == K_d:
             user = "d"
             print("Hello David")
-    screen.fill(backGround)    
     if instruction_page == 1:
-        text = font.render("Please select the first letter of your first name", True, black)
+        text = font.render("Please select the first letter of your first name", True, white)
         screen.blit(text, [10,10])
-        text = font.render("Hit 'j' for 'Jeremy'", True, black)
+        text = font.render("Hit 'j' for 'Jeremy'", True, white)
         screen.blit(text, [10,40])
-        text = font.render("Hit 'r' for 'Riley'", True, black)
+        text = font.render("Hit 'r' for 'Riley'", True, white)
         screen.blit(text, [10,70])
-        text = font.render("Hit 'g' for 'Gabrielle'", True, black)
+        text = font.render("Hit 'g' for 'Gabrielle'", True, white)
         screen.blit(text, [10,100])
-        text = font.render("Hit 'd' for 'David'", True, black)
+        text = font.render("Hit 'd' for 'David'", True, white)
         screen.blit(text, [10,130])
-        text = font.render("Hit the space bar to continue", True, black)
-        screen.blit(text, [10,170])  
+        text = font.render("Hit the space bar to continue", True, white)
+        screen.blit(text, [10,170]) 
+    clock.tick(20)
+    pygame.display.flip() 
 bg_mog = cv2.BackgroundSubtractorMOG()
 
 def segmentBG(img_in, learn_rate):
