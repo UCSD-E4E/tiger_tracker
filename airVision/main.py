7cb45e50785c1b43a75a4b158f4932173022f045
David Muller <davehmuller@gmail.com>
2013-11-23 19:15:18 -0800

    Added a command line parameter for a log file, a function to log debug messages to that log file, and a --verbose option so that output can also be printed in stdout

--- airVision/main.py
+++ airVision/main.py
@@ -10,9 +10,23 @@ import getPositives
 import shutil
 import os
 
-def terminate_main():
-    print "\nExiting at:",str(datetime.datetime.now())
-    print "\n------------------------------------------------------------"
+
+
+# Write a log message to our output file.
+# Params: to_log--the string with the message to log.
+# file_name--name of the log file to write to
+# verbose_arg--the command line arg; should the 
+# message be written to stdout as well?
+def log_message(to_log, file_name, verbose_arg):
+    with open(file_name, "a") as log_file:
+        log_file.write(to_log)    
+    if verbose_arg == True:
+        print to_log
+
+
+def terminate_main(log_file, verbose):
+    log_message("\n\nExiting at: " + str(datetime.datetime.now()), log_file, verbose)
+    log_message("\n------------------------------------------------------------", log_file, verbose)
     exit(0) 
 
 
@@ -39,45 +53,48 @@ def copy_file(abs_dir, new_dir, new_name):
 parser = argparse.ArgumentParser(description='Process airVision security footage for activity.')
 parser.add_argument('video_path', help='Path to airVision Videos directory.')
 parser.add_argument('saved_activity', help='Path to directory to save active videos under.')
+parser.add_argument('log_file', help='File to put logging messages in.')
+parser.add_argument('--verbose', action='store_true', help='If verbose is given, logging messages will be printed to stdout.')
 args = parser.parse_args()  
 
-print "\n------------------------------------------------------------"
-print "\nProcessing airVision data at:", args.video_path
-print "Saving positive footage at:", args.saved_activity
-print "Beginning processing at:",str(datetime.datetime.now())
+log_message("\n\n\n------------------------------------------------------------", args.log_file, args.verbose)
+log_message("\nBeginning: " + str(datetime.datetime.now()), args.log_file, args.verbose)
+log_message("\nProcessing airVision data at: " + args.video_path, args.log_file, args.verbose)
+log_message("\nSaving positive footage at: " + args.saved_activity, args.log_file, args.verbose)
    
 
 # grab the dates of the videos (2013/10/22/13), camera_ids, and absolut paths
 dates, camera_ids, abs_paths = video_retriever.grab_video_dirs(args.video_path)
 
 if len(dates) != len(camera_ids):
-    print "The number of returned dates from the airVision directory, does not equal the number of camera_ids.  Exiting..."
-    terminate_main()
+    log_message("\nThe number of returned dates from the airVision directory, does not equal the number of camera_ids.  Exiting...", args.log_file, args.verbose)
+    terminate_main(args.log_file, args.verbose)
 else:
     date_cams_abspaths = zip(dates, camera_ids, abs_paths)
 
 # create the tiger_log table
 tiger_log.create_table()
 
-print "\nUpdating the rows in the tiger_log.db table..."
+log_message("\n\nUpdating the rows in the tiger_log.db table...", args.log_file, args.verbose)
 
 # update the rows in the table--adding any newly discovered video directories
 tiger_log.insert_many_rows(date_cams_abspaths)
 
-print "Selecting unprocessed rows from tiger_log.db table...\n"
+log_message("\nSelecting unprocessed rows from tiger_log.db table...", args.log_file, args.verbose)
 
 # grab the abs_paths of the directories that need processing
 need_processing = tiger_log.select_unprocessed()
 
 # exit if there's nothing to process
 if len(need_processing) == 0:
-    print "Nothing to process..."    
-    terminate_main()
+    log_message("\nNothing to process...", args.log_file, args.verbose)   
+    terminate_main(args.log_file, args.verbose)
 
 # loop through all directories that need processing
+log_message("\n", args.log_file, args.verbose)
 for item in need_processing:
     currently_processing = item[0]
-    print "Processing this directory:",currently_processing
+    log_message("\nProcessing this directory: " + currently_processing, args.log_file, args.verbose)
     tiger_count = 0    
     counts_and_dirs = getPositives.retClips(currently_processing,500, 10) # dir, min_size, hits
 
@@ -93,9 +110,9 @@ for item in need_processing:
     # update the table for this directory
     tiger_log.update_processed_by_dir(currently_processing, 'Y')
     tiger_log.update_pos_frames_by_dir(currently_processing, tiger_count)
-    print '✓\n'
+    log_message('\n✓', args.log_file, args.verbose)
         
-terminate_main()
+terminate_main(args.log_file, args.verbose)
 
 
 
