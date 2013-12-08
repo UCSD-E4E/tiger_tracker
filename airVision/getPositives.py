f905e13664681029350d01a5c82b730b9a3d93d0
David Muller <davehmuller@gmail.com>
2013-12-07 19:53:44 -0800

    Fixed a bug in the alphanumeric sort...the function now handles stings and tuples without error

--- airVision/getPositives.py
+++ airVision/getPositives.py
@@ -16,7 +16,7 @@ def tryint(s):
     
 def alphanum_key(s):
     # if given a tuple, sort on the first element    
-    if len(s) > 1:
+    if type(s).__name__ == 'tuple':
         s = s[0]
     return [ tryint(c) for c in re.split('([0-9]+)',s) ]
 
@@ -81,8 +81,5 @@ def count_pos_frames(clip, min_size, num_hits, detector, viewer):
         print 'Could not open video file:', clip
         return -1
     return hits
-
-
-
 
 
