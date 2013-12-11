6ad71b940961388deaab821039b9e7d6270cc58e
David Muller <davehmuller@gmail.com>
2013-12-10 20:09:44 -0800

    Add a function for returning rows that have positive footage based on a given date

--- airVision/tiger_log.py
+++ airVision/tiger_log.py
@@ -231,6 +231,20 @@ def select_dirs_with_pos_footage():
 
 
 
+# Select the rows that have saved  footage
+# and the given date.
+# Return: all the directories that have concatenated footage saved.
+def select_dirs_with_positive_footage(date):
+    conn, cursor = open_data_base()
+    conn.row_factory = sqlite3.Row
+    cursor.execute("SELECT saved_at FROM tiger_log WHERE saved_at IS NOT null and date = ?", [date])
+    directories = cursor.fetchall()
+    close_data_base(conn)
+    return directories
+
+
+
+
 # Select the rows that have saved concatenated footage
 # and the given date.
 # Return: all the directories that have concatenated footage saved.
